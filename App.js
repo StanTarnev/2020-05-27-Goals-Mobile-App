99a8beec6744c4a73d58453735fa28585507e654
Stan Tarnev <stan.tarnev@gmail.com>
2020-06-08 03:51:11 +0100

    Defaulted App.js before introducing navigation

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import InputBar from './components/InputBar';
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      goalInput: '',
-      goals: [
-        { id: 0, title: 'Duolingo', done: false },
-        { id: 1, title: 'Read German', done: false }
-      ]
-    }
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.statusbar}></View>
-
-        <Header title="Goals" />
-
-        <InputBar />
+        <Text>Hello World!</Text>
       </View>
     );
   }
@@ -33,10 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-
-  },
-  statusbar: {
-    backgroundColor: '#FFCE00',
-    height: 36
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
