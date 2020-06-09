d441307a049e95768fb235ac4fe27af12c0e5fbc
Stan Tarnev <stan.tarnev@gmail.com>
2020-06-09 19:30:02 +0100

    Imported react navigation into App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+      <NavigationContainer>{
+        <View style={styles.container}>
+          <Text>Hello World!</Text>
+        </View>
+      }
+      </NavigationContainer>
     );
   }
 }
